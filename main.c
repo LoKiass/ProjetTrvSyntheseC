50e08ea30f6ff948d6562e871200c3cf79a1a972
LoKias_ <diseursmat@gmail.com>
2024-12-23 13:24:45 +0100

    Version 5 : Introduction de Tableaux

--- main.c
+++ main.c
@@ -1,33 +1,66 @@
-//PRJTRV4 : Pointeur
-// Les pointeur peuvent pointer l'adresse d'une variable afin de la recuprer ou d'introduire des données dedans
-// px : Adresse que px pointe (Variable X dans ce cas si)
-// *px: Contenue de l'adresse point" par px
-// &px: Adresse du pointeur PX
+//PRJTRV5 : Introduction de tableau manuellement
+// Plusieurs type de tableau : Char et int, 0 de cloture nécesaire pour fermer la lecture du tableau pour des char
 #include <stdio.h>
 #include <conio.h>
+
 int main(void) {
-    int *px = 0, x = 0;
-    char *pc, c; //
-    printf("Avant aquistions d'une valeurs");
-    printf("--> Adresse de la variable X : %04X\n--> Valeur de X avant aquisitions : %d",&x, x);
-    printf("\n--> Adresse du pointeur PX : %04X\n--> Adresse pointe par px  : %d",&px, px);
+    int TailleTab = 4; //Taille du tableau
+    int Tx[TailleTab], x = 0; // X etant l'indice de notre tableau
+    char Tc[TailleTab+1]; //En comptant le 0 de clôture
+    float Tf[TailleTab];
 
-    px = &x;
-    printf("\n\nApres mise en place du pointeur PX : ");
-    printf("\n--> Adresse de la variable X : %04X\n--> Valeur de X avant aquisitions : %d",&x, x);
-    printf("\n--> Adresse du pointeur PX : %04X\n--> Valeur du contenue du pointeur PX : %d\n--> Adresse pointe par PX : %04X",&px, *px, px);
+    // Entré manuelle d'un tableau d'entier numérique
+    printf("Tableau d'entier");
+    printf("\n-> Valeur N.0 Tableau Entier: ");
+    scanf("%d", &Tx[x]); getchar();
+    x++;
+    printf("\n-> Valeur N.1 Tableau Entier: ");
+    scanf("%d", &Tx[x]); getchar();
+    x++;
+    printf("\n-> Valeur N.2 Tableau Entier: ");
+    scanf("%d", &Tx[x]); getchar();
+    x++;
+    printf("\n-> Valeur N.3 Tableau Entier: ");
+    scanf("%d", &Tx[x]); getchar();
+    x++;
+    printf("\n\t * N.0 valeur : %d\n\t * N.1 valeur : %d\n\t * N.2 valeur : %d\n\t * N.3 valeur : %d", Tx[0], Tx[1], Tx[2], Tx[3]);
+    x = 0;
 
+    //Entre manuelle tableau de charactere
+    printf("Tableau de charactere (Ou chaine de charactere");
+    printf("Tableau d'entier");
+    printf("\n-> Valeur N.0 Tableau Charactere: ");
+    Tc[x] = getch();
+    x++;
+    printf("\n-> Valeur N.1 Tableau Charactere: ");
+    Tc[x] = getch();
+    x++;
+    printf("\n-> Valeur N.2 Tableau Charactere: ");
+    Tc[x] = getch();
+    x++;
+    printf("\n-> Valeur N.3 Tableau Charactere : ");
+    Tc[x] = getch();
+    printf("\n\n\t * N.0 valeur : %c\n\t * N.1 valeur : %c\n\t * N.2 valeur : %c", Tc[0], Tc[1], Tc[2]);
+    printf("\n\n\t * Autres maniere d'ecrire une chaine de charactere mais non manuellement :  %s", Tc);
+    printf("\n\n\t * Derniere case de notre tableau : %d", Tc[3]);
+    x = 0;
 
-    *px = 100;
-    printf("\n\nApres avoir donne la valeur du contenue de px : ");
-    printf("\n--> Adresse de la variable X : %04X\n--> Valeur de X avant aquisitions : %d",&x, x);
-    printf("\n--> Adresse du pointeur PX : %04X\n--> Valeur du contenue du pointeur PX : %d\n--> Adresse pointe par PX : %04X",&px, *px, px);
+    //Entre manuelle tableau float
+    printf("Tableau de float");
+    printf("\n-> Valeur N.0 Tableau float: ");
+    scanf("%f", &Tf[x]); getchar();
+    x++;
+    printf("\n-> Valeur N.1 Tableau float: ");
+    scanf("%f", &Tf[x]); getchar();
+    x++;
+    printf("\n-> Valeur N.2 Tableau float: ");
+    scanf("%f", &Tf[x]); getchar();
+    x++;
+    printf("\n-> Valeur N.3 Tableau float: ");
+    scanf("%f", &Tf[x]); getchar();
+    x++;
+    printf("\n\t * N.0 valeur : %f\n\t * N.1 valeur : %f\n\t * N.2 valeur : %f\n\t * N.3 valeur : %f", Tf[0], Tf[1], Tf[2], Tf[3]);
 
-    x = 200;
-    printf("\n\nApres avoir donne une valeur a X: ");
-    printf("\n--> Adresse de la variable X : %04X\n--> Valeur de X avant aquisitions : %d",&x, x);
-    printf("\n--> Adresse du pointeur PX : %04X\n--> Valeur du contenue du pointeur PX : %d\n--> Adresse pointe par PX : %04X",&px, *px, px);
     getch();
 
-    return 0;
 }
