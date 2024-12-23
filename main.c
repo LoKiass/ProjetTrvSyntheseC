a072874ad7c4f687b8e4f4c52f5dd96689e166f2
LoKias_ <diseursmat@gmail.com>
2024-12-23 18:44:36 +0100

    Version 6 : Boucles

--- main.c
+++ main.c
@@ -1,47 +1,51 @@
-//PrjTrv 5.1 : Gestion avancé d'un tableau de charactère
-// Gestion des touches spéciale comme les touche fonction, flèches et etc.
+//PrjTrv 6 : Boucles
+// Gerer les 3 types de boucles pour rentré de simple valeur dans un tableau
 #include <stdio.h>
 #include <conio.h>
 
 int main(void) {
-    char Tchar[1];
-    int x = 0;
-    Tchar[x] = getch();
-    switch (Tchar[x]) {
-        case 13: // Code ASCII de la touche "Enter"
-            printf("Vous avez appuyer sur la touche Enter");
-        break;
-        case 8: // Code ASCII de la touche "Backspace"
-            printf("Vous avez appuyer sur la touche backspace");
-        break;
-        case 27: // Code ASCII de la touche "Espace"
-            printf("Vous avez appuyer sur la touche ");
-        break;
-        case 9:// Code ASCII de la touche "Tab"
-            printf("Vous avez appuyer sur la touche Tab");
-        break;
-        case 0: // Première parti du code Ascii d'une touche fonction
-            case -32: // Deuxième partir du code Ascii de la touche fonction
-            printf("Vous avez appuyer sur la touche FX");
-        getch(); // On suprimme le code dans le Buffer (2ème parti du code ASCII des touches fonctions
-        break;
-        case 32: // Code Ascii de la touche espace
-            printf("Vous avez appuyer sur la touche Espace");
-        break;
-        default:
-            if (Tchar[x] >= 48 && Tchar[x] <= 57) { // Code Ascii 0-9
-                printf("Vous avez appuyer sur une touche Alpha-numérique");
-            }
-            else if (Tchar[x] >= 65 && Tchar[x] <= 90) { // Code Ascii A-Z
-                printf("Vous avez appuez sur une touche Majuscules");
-            }
-            else if (Tchar[x] >= 97 && Tchar[x] <= 122) { // Code Ascii a-z
-                printf("Vous avez appuer sur une touche Minuscule");
-            }
-            else { // Autre touche
-                printf("Touche non-gere par le programme");
-            }
+    int TailleTab = 10, x = 0;
+    int Tx[TailleTab];
+
+     // Type de boucle 1 : While
+     // Aquisitions des valeurs
+     printf("Aquisitions de valeur boucle WHILE\n");
+     while(x /*0-1-2-3-4-5-6-7-8-9-10*/ != TailleTab /*10*/ ) {
+         printf("\t * Chiffre N.%d : ", x);
+         scanf("%d",&Tx[x]); getchar();
+         x++;
+     }
+     x = 0;
+     printf("Chiffres que nous avons obtenue : ");
+     while(x != TailleTab) {
+         printf("\n\t * Chifres n.%d : %d",x , Tx[x]);
+         x++;
+     }
+
+     x = 0;
+     // Type de boucles 2 : DO WHILE
+     //Aquistions des valeurs
+     printf("\nAquisitions des valeurs boucle DO... WHILE\n");
+     do {
+         printf("\t * Chiffre N.%d : ", x);
+         scanf("%d",&Tx[x]); getchar();
+         x++;
+     }while (x != TailleTab);
+     x = 0;
+     do {
+         printf("\n\t * Chifres n.%d : %d",x , Tx[x]);
+         x++;
+     }while (x != TailleTab);
+
+    // Type de boucle 3 : FOR
+    // Aquisitions des valeurs
+    printf("\nAquisitions des valeurs boucle FOR\n");
+    for (x = 0; x != TailleTab; x++) {
+        printf("\t * Chiffre N.%d : ", x);
+        scanf("%d",&Tx[x]); getchar();
+    }
+    for (x = 0; x != TailleTab; x++) {
+        printf("\n\t * Chifres n.%d : %d",x , Tx[x]);
     }
     getch();
-
 }
